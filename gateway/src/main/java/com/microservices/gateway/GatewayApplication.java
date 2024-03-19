ef567107ce87a97525ac67f9e86d200069c20874
Mfikrilamin <muhd.fikrilamin@gmail.com>
2024-03-19 23:36:28 +0800

    add docker into the file, but still cannot spin up the container

--- gateway/src/main/java/com/microservices/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/microservices/gateway/GatewayApplication.java
@@ -2,6 +2,7 @@ package com.microservices.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 public class GatewayApplication {
